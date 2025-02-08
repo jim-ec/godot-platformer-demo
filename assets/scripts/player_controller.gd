8fc4976a3f63ed6d2cce887cf7aeb0983e61f1c0
Jim Eckerlein <jim.eckerlein@icloud.com>
2025-02-08 14:19:55 +0100

    tweak controls

--- assets/scripts/player_controller.gd
+++ assets/scripts/player_controller.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
-var speed = 150.0
-var jump_power = -250.0
+@export var speed = 10.0
+@export var jump_power = 10.0
 
+var speed_multiplier = 30.0
+var jump_multiplier = -30.0
+var direction = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,14 +15,14 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_power
+		velocity.y = jump_power * jump_multiplier
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+	direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction * speed * speed_multiplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
 	move_and_slide()
