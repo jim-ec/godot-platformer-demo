4b624938cd851dd44ecb1dee6a6a94dd5cef5a76
Jim Eckerlein <jim.eckerlein@icloud.com>
2025-02-09 12:46:19 +0100

    condense level

--- assets/scripts/moving_platform.gd
+++ assets/scripts/moving_platform.gd
@@ -5,7 +5,7 @@ class_name MovingPlatform
 @export var path_time = 1.0
 @export var loops = false
 @export var path_follow: PathFollow2D
-@export var ease: Tween.EaseType
+@export var path_ease: Tween.EaseType
 @export var transition: Tween.TransitionType
 
 
@@ -15,11 +15,11 @@ func _ready() -> void:
 
 func move_tween():
 	var tween = get_tree().create_tween().set_loops()
-	tween.tween_property(path_follow, "progress_ratio", 1, path_time).set_ease(ease).set_trans(transition)
+	tween.tween_property(path_follow, "progress_ratio", 1, path_time).set_ease(path_ease).set_trans(transition)
 	if not loops:
-		tween.tween_property(path_follow, "progress_ratio", 0, path_time).set_ease(ease).set_trans(transition)
+		tween.tween_property(path_follow, "progress_ratio", 0, path_time).set_ease(path_ease).set_trans(transition)
 	else:
-		tween.tween_property(path_follow, "progress_ratio", 0, 0).set_ease(ease).set_trans(transition)
+		tween.tween_property(path_follow, "progress_ratio", 0, 0).set_ease(path_ease).set_trans(transition)
 
 
 func _process(_delta: float) -> void:
