7df9321d9d0646be98a4f46a4affb5c4faf88131
Jim Eckerlein <jim.eckerlein@icloud.com>
2025-02-09 13:55:05 +0100

    enable grid snap, add some energy cells

---
+++ assets/scripts/energy_cell.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+func _on_body_entered(body: Node2D) -> void:
+	
+	if body is PlayerController:
+		GameManager.energy_cells += 1
+		queue_free()
+	
+	pass # Replace with function body.
